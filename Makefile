a91e76c70871d958bb9ac6de45652516b8fc2212
Ervan Lefevre <94843168+esilvert@users.noreply.github.com>
2023-07-31 11:48:33 +0200

    :sparkles: Adds an installation process (README + script + linked fixes) (#1)
    * :memo: :hammer: Adds make install & README
    
    * :hammer: Adds a makefile to root directory
    
    * :bug: Fixes references to 'app' for 'ducky'
    
    * :pencile2: Removes spaces for tabs in Makefile
    
    * :memo: Use submodules in the README
    
    * :recycle: Moves everything in a lib folder for better readability
    
    * :adhesive_bandage: Fixes usages of Colors & Vector
    
    * :hammer: Adds a script to update ducky from Makefile
    
    * :recycle: Installation files are now in a dedicated folder

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-run_game:
-	./dragonruby
+install:
+	cp install/main.rb ../mygame/app/main.rb
+	cp install/repl.rb ../mygame/app/repl.rb
+	cp install/Makefile ../Makefile
---
+++ ducky.rb
@@ -0,0 +1,59 @@
+# Ducky
+
+require 'ducky/lib/support.rb'
+require 'ducky/lib/vector2.rb'
+require 'ducky/lib/color.rb'
+require 'ducky/lib/aabb.rb'
+require 'ducky/lib/input.rb'
+require 'ducky/lib/node.rb'
+require 'ducky/lib/fsm.rb'
+
+require 'ducky/lib/interfaces/uses_mouse.rb'
+
+require 'ducky/lib/root.rb'
+require 'ducky/lib/engines/core.rb'
+require 'ducky/lib/engines/physics.rb'
+
+require 'ducky/lib/nodes/node_2d.rb'
+require 'ducky/lib/nodes/label.rb'
+require 'ducky/lib/nodes/sprite.rb'
+require 'ducky/lib/nodes/button.rb'
+require 'ducky/lib/nodes/physics_2d.rb'
+require 'ducky/lib/nodes/static_body.rb'
+require 'ducky/lib/nodes/kinematic_body.rb'
+require 'ducky/lib/nodes/shape.rb'
+require 'ducky/lib/nodes/rectangle_shape.rb'
+
+require 'ducky/lib/game.rb'
+require 'ducky/lib/scene.rb'
+
+class Object
+  def log(msg)
+    p "[#{self.class.name}] #{msg}"
+  end
+end
+
+module Ducky
+  SCREEN_WIDTH  = 1280
+  SCREEN_HEIGHT = 720
+  DELTA_TIME = 1.0 / 60.0
+
+  def self.configure
+    Root.new
+  end
+
+  def self.reload
+    p '[Ducky] Reloading the whole game'
+    $ducky = nil
+    clear_all
+  end
+
+  def self.clear_all
+    Ducky::Scene.clear
+    Input.clear
+    Engine::Physics.clear
+    $gtk.stop_music # DEPRECATED
+  end
+end
+
+p '[Ducky] Source files have been loaded'
